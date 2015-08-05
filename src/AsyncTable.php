30e36c1d85b74a180c1f51fd69b90eef3e449dc9
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-08-05 08:00:46 +0200

    Split the calling of methods up in an async and sync method

--- src/AsyncTable.php
+++ src/AsyncTable.php
@@ -2,6 +2,8 @@
 
 namespace WyriHaximus\React\Cake\Orm;
 
+use Cake\ORM\TableRegistry;
+
 class AsyncTable
 {
     /**
@@ -25,6 +27,25 @@ class AsyncTable
     }
 
     public function __call($function, array $arguments = [])
+    {
+        return $this->callAsync($function, $arguments);
+    }
+
+    protected function callSync($function, array $arguments = [])
+    {
+
+        return \React\Promise\resolve(
+                call_user_func_array(
+                [
+                    TableRegistry::get($this->tableName),
+                    $function
+                ],
+                $arguments
+            )
+        );
+    }
+
+    protected function callAsync($function, array $arguments = [])
     {
         $unSerialize = function ($input) {
             return unserialize($input);

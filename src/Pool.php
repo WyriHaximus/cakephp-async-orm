d08d7a2ac3b4e3be4a66a1340855f46f053be8a5
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-08-08 23:48:47 +0200

    Missing docblocks

--- src/Pool.php
+++ src/Pool.php
@@ -9,11 +9,25 @@ use WyriHaximus\React\ChildProcess\Messenger\Messages\Call;
 use WyriHaximus\React\ChildProcess\Messenger\Messages\Factory;
 use WyriHaximus\React\ChildProcess\Pool\FlexiblePool;
 
+/**
+ * Class Pool
+ * @package WyriHaximus\React\Cake\Orm
+ */
 class Pool
 {
+    /**
+     * @var LoopInterface
+     */
     protected $loop;
+
+    /**
+     * @var FlexiblePool
+     */
     protected $pool;
 
+    /**
+     * @param LoopInterface $loop
+     */
     protected function __construct(LoopInterface $loop)
     {
         $this->loop = $loop;
@@ -28,6 +42,10 @@ class Pool
         );
     }
 
+    /**
+     * @param LoopInterface $loop
+     * @return static
+     */
     public static function getInstance(LoopInterface $loop = null)
     {
         static $instance = null;
@@ -38,6 +56,12 @@ class Pool
         return $instance;
     }
 
+    /**
+     * @param $tableName
+     * @param $function
+     * @param array $arguments
+     * @return \React\Promise\PromiseInterface
+     */
     public function call($tableName, $function, array $arguments)
     {
         return $this->pool->rpc(Factory::rpc('table.call', [

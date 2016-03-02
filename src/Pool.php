19a103d5b7e001ffb5e21098199bc16f3398ad0a
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-03-02 22:26:32 +0100

    Fixed race condition

--- src/Pool.php
+++ src/Pool.php
@@ -5,6 +5,9 @@ namespace WyriHaximus\React\Cake\Orm;
 use Cake\Core\Configure;
 use React\ChildProcess\Process;
 use React\EventLoop\LoopInterface;
+use React\EventLoop\Timer\TimerInterface;
+use React\Promise\Deferred;
+use React\Promise\PromiseInterface;
 use WyriHaximus\React\ChildProcess\Messenger\Messages\Factory;
 use WyriHaximus\React\ChildProcess\Pool\Pool\Flexible;
 use WyriHaximus\React\ChildProcess\Pool\PoolInfoInterface;
@@ -72,13 +75,37 @@ class Pool implements PoolUtilizerInterface
      */
     public function call($tableName, $function, array $arguments)
     {
-        return $this->pool->rpc(Factory::rpc('table.call', [
-            'function' => $function,
-            'table' => $tableName,
-            'arguments' => serialize($arguments),
-        ]))->then(function ($result) {
-            return \React\Promise\resolve($result['result']);
+        if ($this->pool instanceof PoolInterface) {
+            return $this->pool->rpc(Factory::rpc('table.call', [
+                'function' => $function,
+                'table' => $tableName,
+                'arguments' => serialize($arguments),
+            ]))->then(function ($result) {
+                return \React\Promise\resolve($result['result']);
+            });
+        }
+
+        return $this->waitForPoolCall($tableName, $function, $arguments);
+    }
+
+    /**
+     * @param $tableName
+     * @param $function
+     * @param array $arguments
+     * @return PromiseInterface
+     */
+    protected function waitForPoolCall($tableName, $function, array $arguments)
+    {
+        $deferred = new Deferred();
+
+        $this->loop->addPeriodicTimer(0.1, function (TimerInterface $timer) use ($deferred, $tableName, $function, $arguments) {
+            if ($this->pool instanceof PoolInterface) {
+                $timer->cancel();
+                $deferred->resolve($this->call($tableName, $function, $arguments));
+            }
         });
+
+        return $deferred->promise();
     }
 
     /**

f202f5d9dc9894cda3dbb99edf5494c4d8008e47
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-10-15 13:25:05 +0200

    Test against generated table object

--- tests/AsyncTableGeneratorTest.php
+++ tests/AsyncTableGeneratorTest.php
@@ -3,6 +3,7 @@
 namespace WyriHaximus\React\Tests\Cake\Orm;
 
 use WyriHaximus\React\Cake\Orm\AsyncTableGenerator;
+use WyriHaximus\React\Cake\Orm\GeneratedTable;
 use WyriHaximus\React\TestApp\Cake\Orm\Table\ScreenshotsTable;
 
 class AsyncTableGeneratorTest extends TestCase
@@ -12,10 +13,11 @@ class AsyncTableGeneratorTest extends TestCase
         $tmpDir = sys_get_temp_dir() . DS . uniqid('WyriHaximus-Cake-Async-ORM-', true) . DS;
         mkdir($tmpDir, 0777, true);
         $generator = new AsyncTableGenerator($tmpDir);
-        $filename = $generator->generate(ScreenshotsTable::class);
+        $generatedTable = $generator->generate(ScreenshotsTable::class);
+        $this->assertInstanceOf(GeneratedTable::class, $generatedTable);
         $this->assertFileEquals(
             dirname(__DIR__) . DS . 'test_app' . DS . 'ExpectedGeneratedAsyncTable' . DS . 'C17a66dcf052f6878c3f1c553db4d6bd0_Ff47f6f78cf1b377de64788b3705cda9c.php',
-            $tmpDir . $filename . '.php'
+            $tmpDir . $generatedTable->getClassName() . '.php'
         );
     }
 }

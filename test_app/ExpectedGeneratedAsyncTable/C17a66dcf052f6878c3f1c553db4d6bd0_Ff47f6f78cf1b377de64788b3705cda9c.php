be85ec245420453a554fcfa9372d6a59a57433ed
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-03-14 17:25:13 +0100

    Fixed expected generated able outcome

--- test_app/ExpectedGeneratedAsyncTable/C17a66dcf052f6878c3f1c553db4d6bd0_Ff47f6f78cf1b377de64788b3705cda9c.php
+++ test_app/ExpectedGeneratedAsyncTable/C17a66dcf052f6878c3f1c553db4d6bd0_Ff47f6f78cf1b377de64788b3705cda9c.php
@@ -1,24 +1,33 @@
-<?php
+<?php declare(strict_types=1);
 
-namespace WyriHaximus\GeneratedAsyncCakeTable\WyriHaximus\React\TestApp\Cake\Orm\Table;
+namespace WyriHaximus\GeneratedAsyncCakeTable;
 
 use Cake\Datasource\EntityInterface;
-use WyriHaximus\React\TestApp\Cake\Orm\Table\ScreenshotsTable as BaseTable;
 use WyriHaximus\React\Cake\Orm\AsyncTable;
 use WyriHaximus\React\Cake\Orm\AsyncTableInterface;
-class C17a66dcf052f6878c3f1c553db4d6bd0_Ff47f6f78cf1b377de64788b3705cda9c extends BaseTable implements AsyncTableInterface
+use WyriHaximus\React\TestApp\Cake\Orm\Table\ScreenshotsTable as BaseTable;
+
+class WyriHaximus_React_TestApp_Cake_Orm_Table_C17a66dcf052f6878c3f1c553db4d6bd0_F53497c4a0f2baa918de7ec31e3ec7f23 extends BaseTable implements AsyncTableInterface
 {
     use AsyncTable;
-    public function save(EntityInterface $entity, $options = array())
+
+    public function save(EntityInterface $entity, $options = [])
     {
-        return $this->callAsyncOrSync('save', array($entity, $options));
+        return $this->callAsyncOrSync('save', [$entity, $options]);
     }
+
     public function fetchNewest()
     {
-        return $this->callAsyncOrSync('fetchNewest', array());
+        return $this->callAsyncOrSync('fetchNewest', []);
     }
+
     public function getByMd5($md5)
     {
-        return $this->callAsyncOrSync('getByMd5', array($md5));
+        return $this->callAsyncOrSync('getByMd5', [$md5]);
+    }
+
+    public function getByEntity($md5)
+    {
+        return $this->callAsyncOrSync('getByEntity', [$md5]);
     }
 }
